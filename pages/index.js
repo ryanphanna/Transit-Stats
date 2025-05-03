421225c64fd5ff47f878e502fa8607b53d4efcf6
Ryan Hanna <36575799+ryanphanna@users.noreply.github.com>
2025-05-03 18:47:37 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
+import TripForm from '../components/TripForm'
+
 export default function Home() {
   return (
-    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
-      <h1>Welcome to Transit Stats</h1>
-      <p>This is a clean slate!</p>
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-4">Transit.Stats</h1>
+      <TripForm />
     </div>
-  );
+  )
 }
