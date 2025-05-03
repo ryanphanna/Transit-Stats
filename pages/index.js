ec4ddf1f22c53ad24183488c4ff90d67675c7b34
Ryan Hanna <36575799+ryanphanna@users.noreply.github.com>
2025-05-03 18:37:43 -0400

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>Welcome to Transit Stats</h1>
+      <p>This is a clean slate!</p>
+    </div>
+  );
+}
